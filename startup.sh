292d17a5b3ae96158b55bea11413020f325cdf1a
Nick Humrich <nick.humrich@gmail.com>
2016-08-29 11:20:04 -0600

    Try to fix wercker

--- startup.sh
+++ startup.sh
@@ -3,4 +3,10 @@
 echo ---Running serf---
 
 ./migrate-db.sh
-python3 -u run.py
+
+if [ "$NO_GUNICORN" = "true" ];
+then
+  python3 -u run.py
+else
+  gunicorn run:my_app --bind 0.0.0.0:9191 --worker-class aiohttp.worker.GunicornUVLoopWebWorker -w ${NUM_WORKERS:-2} --timeout 0
+fi
