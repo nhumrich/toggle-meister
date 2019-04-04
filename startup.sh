30600adccd8df9d3e5e5199b54f38b5ac3fa75c5
Nick Humrich <nick.humrich@gmail.com>
2019-04-04 14:19:45 -0600

    Fix startup command

--- startup.sh
+++ startup.sh
@@ -5,10 +5,4 @@ then
 ./migrate-db.sh
 fi
 
-
-if [ "$NO_GUNICORN" = "true" ];
-then
-  python3 -u run.py
-else
-  gunicorn run:app --bind 0.0.0.0:8445 --worker-class aiohttp.worker.GunicornUVLoopWebWorker -w ${NUM_WORKERS:-2} --timeout 0
-fi
+exec python3 -u run.py
