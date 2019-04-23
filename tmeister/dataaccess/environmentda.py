9ca94902caaaebc260960b05405dcb1933fa228d
Nick Humrich <nick.humrich@gmail.com>
2019-04-23 11:20:02 -0600

    Fix setting toggles during phase 1
    also: fix chicken switch on production

--- tmeister/dataaccess/environmentda.py
+++ tmeister/dataaccess/environmentda.py
@@ -1,4 +1,5 @@
 from asyncpgsa import pg
+from sqlalchemy import func
 
 from . import db
 
@@ -11,7 +12,7 @@ async def add_env(env_name):
 async def get_envs(*, env_list=None):
     query = db.environments.select()
     if env_list:
-        query = query.where(db.environments.c.name.in_(env_list))
+        query = query.where(func.lower(db.environments.c.name).in_(env_list))
 
     envs = await pg.fetch(query)
     results = []
