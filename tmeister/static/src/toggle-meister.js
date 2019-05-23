f548146868021c460dcb701becef6a9de7b3cc14
Justin McMurdie <mcmurdiej@gmail.com>
2019-05-23 11:04:20 -0600

    removing last bits of redux code

--- tmeister/static/src/toggle-meister.js
+++ tmeister/static/src/toggle-meister.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { applyMiddleware, createStore, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
 
 import RootRoutes from './root.routes.js';
-import toggleReducer from './toggles/toggles.reducer.js';
 
-const reducer = combineReducers({
-	toggles: toggleReducer,
-});
-const store = createStore(reducer, applyMiddleware(thunk));
-
-ReactDom.render(<RootRoutes store={store}/>, document.getElementById('main-content'));
+ReactDom.render(<RootRoutes />, document.getElementById('main-content'));
