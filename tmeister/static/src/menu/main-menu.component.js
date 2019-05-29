75760d77528587e70f7c5767349c7c8a8ba87abe
Justin McMurdie <mcmurdiej@gmail.com>
2019-05-29 14:04:54 -0600

    More material

--- tmeister/static/src/menu/main-menu.component.js
+++ tmeister/static/src/menu/main-menu.component.js
@@ -1,24 +1,45 @@
 import React from 'react';
 import styles from './main-menu.styles.css';
-import CprTooltip from 'cpr-tooltip';
+import { Tooltip, Paper } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: theme.palette.primary[400]
+  },
+  menuItem: {
+    textDecoration: 'none',
+    '&.active' : {
+      backgroundColor: theme.palette.primary[700]
+    }
+  }
+}))
 
 export default function MainMenu(props) {
+  const classes = useStyles()
 	return (
-		<div className={`${styles.menu}`}>
-			<CprTooltip text="Manage toggles">
-				<a className={`${styles.menuItem} ${activeIcon("toggles")}`} title="Manage toggles" href="/#/toggles">
+		<Paper className={`${styles.menu} `} classes={classes}>
+			<Tooltip title="Manage toggles">
+        <a
+          className={`${styles.menuItem} ${classes.menuItem} ${activeIcon("toggles")}`}
+          title="Manage toggles"
+          href="/#/toggles"
+        >
 					<img src='/resources/toggle-icon.png' className={`${styles.icon}`} />
 				</a>
-			</CprTooltip>
-			<CprTooltip text="Audit trail">
-				<a className={`${styles.menuItem} ${activeIcon("audit-trail")}`} href="/#/audit-trail">
+			</Tooltip>
+			<Tooltip title="Audit trail">
+        <a
+          className={`${styles.menuItem} ${classes.menuItem} ${activeIcon("audit-trail")}`}
+          href="/#/audit-trail"
+        >
 					<img src='/resources/audit-icon.png' className={`${styles.icon}`} />
 				</a>
-			</CprTooltip>
-		</div>
+			</Tooltip>
+		</Paper>
 	);
 }
 
 function activeIcon(urlFragment) {
-	return window.location.hash.indexOf(urlFragment) >= 0 ? styles.active : '';
+	return window.location.hash.indexOf(urlFragment) >= 0 ? 'active' : '';
 }
--- tmeister/static/src/root.routes.js
+++ tmeister/static/src/root.routes.js
@@ -3,18 +3,30 @@ import { Router, Route, Link, Redirect, hashHistory } from 'react-router';
 import TogglesContainer from './toggles/toggles.container.js';
 import RootContainer from './menu/root.container.js';
 import AuditTrail from './audit-trail/audit-trail.container.js';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles'
+import { green, blue } from '@material-ui/core/colors'
+console.log('green', green)
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+    secondary: blue,
+  }
+})
 
 export default class RootComponent extends React.Component {
 
   render() {
-
     return (
-      <Router history={hashHistory}>
-        <Route path="/" component={RootContainer}>
-          <Route path="audit-trail" component={AuditTrail} />
-          <Route path="toggles" component={TogglesContainer} />
-        </Route>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router history={hashHistory}>
+          <Route path="/" component={RootContainer}>
+            <Route path="audit-trail" component={AuditTrail} />
+            <Route path="toggles" component={TogglesContainer} />
+          </Route>
+        </Router>
+      </ThemeProvider>
     );
   }
 }
