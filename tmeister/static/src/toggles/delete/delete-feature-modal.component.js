5a44ebd09b0e16e854a3352ef70719b12dc8eacc
Justin McMurdie <mcmurdiej@gmail.com>
2019-05-29 09:55:05 -0600

    modals 2

--- tmeister/static/src/toggles/delete/delete-feature-modal.component.js
+++ tmeister/static/src/toggles/delete/delete-feature-modal.component.js
@@ -10,43 +10,35 @@ export default function DeleteFeatureModal (props) {
   const metricsActive = useLocalStorageAsToggle('metrics')
   const [metrics, loading, refetch]= useMetricsForToggle(metricsActive ? featureName : undefined)
   return (
-    <Modal>
-      <div className="cps-modal">
-        <div className="cps-modal__screen"></div>
-        <div className="cps-modal__dialog cps-card">
-          <div className="cps-card__header cps-subheader-sm">
-            <span>
-              Delete feature '{featureName}'
-            </span>
-            <a className="cps-modal__dialog__close cps-icon cps-icon-close" onClick={close} />
-          </div>
-          <div className="cps-card__body">
-            <div>
-              This is a hard delete. No takebacks. You should verify that the feature toggle is not referenced in code anymore before
-              doing this. If it is referenced in code after this is deleted, the toggle meister will return a value of "OFF" for this
-              feature toggle after the delete occurs.
-            </div>
-            {
+    <Modal
+      headerText={`Delete feature ${featureName}`}
+      closeAction={close}
+    >
+      <div>
+        <div>
+          This is a hard delete. No takebacks. You should verify that the feature toggle is not referenced in code anymore before
+          doing this. If it is referenced in code after this is deleted, the toggle meister will return a value of "OFF" for this
+          feature toggle after the delete occurs.
+        </div>
+        {
 
-              metricsActive && (
-                <>
-                <br />
-                <DeleteModalMetrics
-                  metrics={metrics}
-                  loading={loading}
-                />
-                </>
-              )
-            }
-          </div>
-          <div className="cps-modal__dialog__actions">
-            <SimpleButton actionType="primary" onClick={performDelete} disableOnClick={true}>
-              Delete feature toggle
-            </SimpleButton>
-            <SimpleButton actionType="flat" onClick={close}>
-              Nevermind
-            </SimpleButton>
-          </div>
+          metricsActive && (
+            <>
+              <br />
+              <DeleteModalMetrics
+                metrics={metrics}
+                loading={loading}
+              />
+            </>
+        )
+        }
+        <div className="cps-modal__dialog__actions">
+          <SimpleButton actionType="primary" onClick={performDelete} disableOnClick={true}>
+            Delete feature toggle
+          </SimpleButton>
+          <SimpleButton actionType="flat" onClick={close}>
+            Nevermind
+          </SimpleButton>
         </div>
       </div>
     </Modal>
