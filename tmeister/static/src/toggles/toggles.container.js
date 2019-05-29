467394297f14a552e22b7a664ca4a13b13720127
Justin McMurdie <mcmurdiej@gmail.com>
2019-05-29 11:56:30 -0600

    Material search

--- tmeister/static/src/toggles/toggles.container.js
+++ tmeister/static/src/toggles/toggles.container.js
@@ -3,31 +3,45 @@ import ToggleControls from './toggle-controls.component.js';
 import ToggleTable from './table/toggle-table.component.js';
 import styles from './toggles.container.css';
 import { useFetchToggles, useFilterToggles } from './toggles.hooks.js'
+import { Container, Paper, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  header: {
+    marginBottom: '8px'
+  },
+  controls: {
+    marginBottom: '16px'
+  }
+}))
 
 export default function TogglesContainer (props) {
+  const classes = useStyles()
   const [ search, setSearch ] = useState()
   const [ toggles, refetch ] = useFetchToggles()
   const [ envs, setEnvs ] = useState(['production'])
   const filteredToggles = useFilterToggles(envs, toggles, search)
 
   return (
-    <div className={`${styles.root}`}>
-      <h2>
-        Manage feature toggles
-      </h2>
-      <div className={`cps-card cps-padding-16`}>
-        <ToggleControls
-          toggles={toggles}
-          searchToggles={setSearch}
-          refetchToggles={refetch}
-          setEnvs={setEnvs}
-          envs={envs}
-        />
+    <Container className={`${styles.root}`}>
+      <Paper>
+        <Typography className={classes.header} variant='h2'>
+          Manage feature toggles
+        </Typography>
+        <div className={classes.controls}>
+          <ToggleControls
+            toggles={toggles}
+            searchToggles={setSearch}
+            refetchToggles={refetch}
+            setEnvs={setEnvs}
+            envs={envs}
+          />
+        </div>
         <ToggleTable
           toggles={filteredToggles}
           refetchToggles={refetch}
         />
-      </div>
-    </div>
+      </Paper>
+    </Container>
   );
 }
