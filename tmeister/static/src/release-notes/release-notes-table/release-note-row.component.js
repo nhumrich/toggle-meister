49cf3fe7b26ab227490fb5c1cfe69ee39d3315ab
Justin McMurdie <mcmurdiej@gmail.com>
2019-06-05 13:54:16 -0600

    Delete modal

--- tmeister/static/src/release-notes/release-notes-table/release-note-row.component.js
+++ tmeister/static/src/release-notes/release-notes-table/release-note-row.component.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import { TableRow, TableCell, TableBody, Paper, IconButton, Icon } from '@material-ui/core'
 import CreateEditReleaseNote from '../create-edit/create-edit.component.js'
 import ReleaseNoteInlinePreview from './release-note-inline-preview.component.js'
+import DeleteReleaseNote from '../delete/delete-note.component.js'
 
 const useStyles = makeStyles(theme => ({
   flex: {
@@ -15,6 +16,7 @@ export default function ReleaseNoteRow (props) {
   const classes = useStyles()
   const { title, body, relatedToggles } = note
   const [edit, setEdit] = useState(false)
+  const [ showDelete, setDelete ] = useState(false)
   return (
     <TableRow key={note.id}>
       <TableCell>
@@ -35,7 +37,7 @@ export default function ReleaseNoteRow (props) {
               edit
             </Icon>
           </IconButton>
-          <IconButton onClick={() => console.log('delete')}>
+          <IconButton onClick={() => setDelete(!showDelete)}>
             <Icon>
               delete
             </Icon>
@@ -47,6 +49,14 @@ export default function ReleaseNoteRow (props) {
           <CreateEditReleaseNote
             releaseNote={note}
             close={() => setEdit(false)}
+          />
+        )
+      }
+      {
+        showDelete && (
+          <DeleteReleaseNote
+            note={note}
+            close={() => setDelete(false)}
           />
         )
       }
