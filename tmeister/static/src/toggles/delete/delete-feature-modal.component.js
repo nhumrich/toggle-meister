3d1ec8e76fa4673d9410037cb09b4ad577713ef0
Justin McMurdie <mcmurdiej@gmail.com>
2019-06-06 10:50:48 -0600

    Common on delete feature

--- tmeister/static/src/toggles/delete/delete-feature-modal.component.js
+++ tmeister/static/src/toggles/delete/delete-feature-modal.component.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles'
 import Button from 'commonButton'
-import Modal from 'common/modal/modal.component.js'
+import Modal from 'common/modal/scroll-modal.component.js'
 import DeleteModalMetrics from './delete-modal-metrics.component.js'
 import { useMetricsForToggle } from '../metrics/metrics.hooks.js'
+
+const useStyles = makeStyles(theme => ({
+  buttonRow: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+}))
 
 export default function DeleteFeatureModal (props) {
   const { close, featureName, performDelete } = props
   const [metrics, loading, refetch]= useMetricsForToggle(featureName)
+  const c = useStyles()
   return (
     <Modal
       headerText={`Delete feature ${featureName}`}
       closeAction={close}
     >
-      <div>
+      <Modal.Body>
         <div>
           This is a hard delete. No takebacks. You should verify that the feature toggle is not referenced in code anymore before
           doing this. If it is referenced in code after this is deleted, the toggle meister will return a value of "OFF" for this
@@ -23,16 +32,17 @@ export default function DeleteFeatureModal (props) {
           metrics={metrics}
           loading={loading}
         />
-        <br />
-        <div className="cps-modal__dialog__actions">
-          <Button actionType="primary" onClick={performDelete} disableOnClick={true}>
+      </Modal.Body>
+      <Modal.BottomRow>
+        <div className={c.buttonRow}>
+          <Button color="primary" onClick={performDelete} disabled={loading} showLoader={loading} >
             Delete feature toggle
           </Button>
-          <Button actionType="flat" onClick={close}>
+          <Button variant="text" onClick={close}>
             Nevermind
           </Button>
         </div>
-      </div>
+      </Modal.BottomRow>
     </Modal>
   )
 }
